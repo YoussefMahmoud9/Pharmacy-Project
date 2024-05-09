634ffa602a0c15d068d4878d56a949b25cc98c5a
YoussefMahmoud9 <youssefmahmoud102000@gmail.com>
2024-05-09 20:21:34 +0300

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tbl_products]
+(
+	[productName] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [price] INT NULL, 
+    [location] NVARCHAR(50) NULL, 
+    [quantity] INT NULL, 
+    [soled] INT NULL
+)
